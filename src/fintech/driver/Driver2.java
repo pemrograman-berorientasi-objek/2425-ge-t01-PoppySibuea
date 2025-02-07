8ef41897d6b31f2fb0da36adeb9cdeecd245acf9
PoppySibuea <143679506+PoppySibuea@users.noreply.github.com>
2025-02-07 17:27:38 +0700

    pair 8

--- src/fintech/driver/Driver2.java
+++ src/fintech/driver/Driver2.java
@@ -1,6 +1,9 @@
 package fintech.driver;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.Locale;
 
 import fintech.model.Account;
 import fintech.model.Transaction;
@@ -15,28 +18,35 @@ public class Driver2 {
 
         // codes
         Scanner sc = new Scanner(System.in);  
-        String command = sc.nextLine();
+        sc.useLocale(Locale.US);
+        Map<String, Account> akun = new HashMap<>();
 
+    while(true){
+        String command = sc.nextLine();
         if(command.equals("create-account")){
             String owner = sc.nextLine();
             String accountname = sc.nextLine();
             Account create = new Account (owner, accountname);
+            akun.put(accountname, create);
             System.out.println(create.toString());
         }
         else if(command.equals("create-transaction")){
             String accountname = sc.nextLine();
+            Account account = akun.get(accountname);
             Double amount = sc.nextDouble();
             sc.nextLine();
             String postedat = sc.nextLine();
             String note = sc.nextLine();
-            Account account = new Account("owner", accountname);
             Transaction create = new Transaction(account, amount, postedat, note);
+            create.sumamount(amount);
             System.out.println(create.toString());
         }
         else{
             System.out.println("Invalid command");
+            break;
         }
-        sc.close();
+    }
+    sc.close();
     }
 
 }
